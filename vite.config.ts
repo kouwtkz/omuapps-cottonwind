c28986ab7aa653efef7dd9903038781aa072612e
わたかぜコウ <kouwtkz@gmail.com>
2026-07-28 16:02:31 +0900

    adapter-autoに戻した

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -14,9 +14,7 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter({
-				fallback: '404.html'
-			}),
+			adapter: adapter(),
 		})
 	],
 	ssr: {
