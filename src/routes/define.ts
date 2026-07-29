f66c9a3850f25c9c1838972d6ac4a280c21e494e
わたかぜコウ <kouwtkz@gmail.com>
2026-07-29 13:17:48 +0900

    ORIGINに余分なスラッシュがあったのを消した

--- src/routes/define.ts
+++ src/routes/define.ts
@@ -1,4 +1,4 @@
 import { dev } from "$app/environment";
 
-export const ORIGIN = dev ? 'http://localhost:5173' : 'https://omuapps.cottonwind.com/' // 公開先のOrigin (開発時はローカルサーバーのOriginを指定する)
+export const ORIGIN = dev ? 'http://localhost:5173' : 'https://omuapps.cottonwind.com' // 公開先のOrigin (開発時はローカルサーバーのOriginを指定する)
 export const NAMESPACE = 'com.cottonwind.omuapps' // 公開先の逆順ドメイン
